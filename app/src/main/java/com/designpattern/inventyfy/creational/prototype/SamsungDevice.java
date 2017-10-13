1910fc69cc984474beaf5e85a69fde05f7c4784a
Drup Desai <drup.desai@in.fujitsu.com>
2017-10-13 13:58:01 +0530

    DD : Prototype pattern description added and links modified from github.

--- app/src/main/java/com/designpattern/inventyfy/creational/prototype/SamsungDevice.java
+++ app/src/main/java/com/designpattern/inventyfy/creational/prototype/SamsungDevice.java
@@ -4,6 +4,24 @@ package com.designpattern.inventyfy.creational.prototype;
  * Created by desaidr
  */
 
+/**
+ * Prototype pattern is used when the Object creation is a costly affair and requires a lot of time
+ * and resources and you have a similar object already existing.
+ *
+ * Pattern provides a mechanism to copy the original object to the new object and then modify it
+ * accordingly.
+ *
+ * This design pattern mandates that the object which you are copying should provide the copying feature.
+ * It should not be done by any other class. However whether to use <b>shallow</b> or <b>deep</b> copy
+ * of the object properties depends on the requirements and its design decision.
+ *
+ * When to use?
+ * <li>When the classes to instantiate are specified at run time. i.e by dynamic loading.</li>
+ * <li>To avoid building a class hierarchy of factories that parallel the class hierarchy of products.</li>
+ * <li>When instance of a class can have one of only a few different combination of state.
+ * It may be more convenient to install a corresponding number of prototypes and clone them rather than
+ * instantiating the class manually, each time with appropriate state.</li>
+ */
 public class SamsungDevice implements Cloneable {
 
     private String deviceName;
--- app/src/main/java/com/designpattern/inventyfy/utils/LinkConstants.java
+++ app/src/main/java/com/designpattern/inventyfy/utils/LinkConstants.java
@@ -17,6 +17,6 @@ public class LinkConstants {
     public static final String SINGLETON_ENUM_CLASS = "https://github.com/drdesai03/DesingPatterns/blob/master/app/src/main/java/com/designpattern/inventyfy/creational/singleton/SingletonEunm.java";
     public static final String FACTORY_PATTERN = "https://github.com/drdesai03/DesingPatterns/tree/master/app/src/main/java/com/designpattern/inventyfy/creational/facorypattern";
     public static final String ABSTRACT_FACTORY_PATTERN = "https://github.com/drdesai03/DesingPatterns/tree/master/app/src/main/java/com/designpattern/inventyfy/creational/abstractfactorypatternw";
-    public static final String BUILDER_PATTERN = "";
-    public static final String PROTOTYPE_PATTERN = "";
+    public static final String BUILDER_PATTERN = "https://github.com/drdesai03/DesingPatterns/tree/master/app/src/main/java/com/designpattern/inventyfy/creational/builder";
+    public static final String PROTOTYPE_PATTERN = "https://github.com/drdesai03/DesingPatterns/tree/master/app/src/main/java/com/designpattern/inventyfy/creational/prototype";
 }
